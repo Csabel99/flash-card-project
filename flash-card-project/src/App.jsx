6975f08954f0ed248e92f105929b0484a049f9c6
Abel Tapia <tapia.abel23@gmail.com>
2026-03-14 16:51:08 -0400

    Complete flashcards project with streak feature

--- flash-card-project/src/App.jsx
+++ flash-card-project/src/App.jsx
@@ -17,32 +17,87 @@ const App = () => {
   ];
 
   const [currentCard, setCurrentCard] = useState(0);
-  const [isFlipped, setIsFlipped] = useState(false);
 
-  const flipCard = () => {
+  const [guess, setGuess] = useState("");
+  const [result, setResult] = useState("");
 
-    setIsFlipped(!isFlipped);
+  const [currentCounter, setCurrentCounter] = useState(0);
+  const [longestCounter, setLongestCounter] = useState(0);
+
+
+  const matching = (text) => {
+    return text.toLowerCase().trim();
   }
 
-  const updateCard = () => {
-    const randomIndex = Math.floor(Math.random() * cards.length);
-      setCurrentCard(randomIndex);
-      setIsFlipped(false);
+  const checkAnswer = () => {
+    const userGuess = matching(guess);
+    const correctAnswer = matching(cards[currentCard].answer);
+
+    if(userGuess == correctAnswer) {
+      setResult("Correct");
+
+      const newCounter = currentCounter + 1;
+      setCurrentCounter(newCounter);
+      
+      if(newCounter > longestCounter) {
+        setLongestCounter(newCounter);
+      }
+    } else {
+      setResult("Incorrect");
+      setCurrentCounter(0);
+    }
+  }
+
+  const nextCard = () => {
+    if (currentCard < cards.length - 1) {
+      setCurrentCard(currentCard + 1);
+      setGuess("");
+      setResult("");
+  }
+}
+
+  const prevCard =() => {
+    if(currentCard > 0) {
+      setCurrentCard(currentCard - 1);
+      setGuess("");
+      setResult("");
+    }
   }
 
   return (
-
     <div className="App">
       <div className="header">
         <h1>Spanish Flashcards</h1>
         <p>Practice Spanish 101</p>
         <h2>Number of cards: {cards.length}</h2>
       </div>
-        <div className="card" onClick={flipCard}>
-          {isFlipped ? cards[currentCard].answer : cards[currentCard].question}
+        <div className="card">
+          {cards[currentCard].question}
         </div>
 
-        <button onClick={updateCard}>Next Card</button>
+        <input
+          type = "text"
+          placeholder="Enter your guess"
+          value={guess}
+          onChange={(e) => setGuess(e.target.value)}
+        />
+
+        <button onClick={checkAnswer}>
+          Submit Guess 
+        </button>
+
+        <h2>{result}</h2>
+        <h3>Current Streak: {currentCounter}</h3>
+        <h3>Longest Streak: {longestCounter}</h3>
+    
+
+        <button onClick={prevCard} disabled={currentCard === 0}>
+        Previous
+        </button>
+
+      <button onClick={nextCard} disabled={currentCard === cards.length - 1}>
+        Next
+      </button>
         
     </div>
   )
